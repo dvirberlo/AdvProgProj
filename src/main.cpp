1ab0b9ac91027f9422163e3be04ba9ac6d36b991
Dor Shroitman <133168649+dorshroitman0812@users.noreply.github.com>
2024-11-20 16:02:04 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
-
+#include "ICommand.h"
+#include "IMenu.h"
+#include "App.h"
+#include "Help.h"
+#include "Help.h"
+#include "ConsoleMenu.h"
+#include "CommandParser.h"
+#include <string>
+#include <map>
 using namespace std;
-
-int main() {
-    cout << "Hello, world!" << endl;
-    return 0;
+int main()
+{
+    map<string, ICommand *> commands;
+    commands["help"] = new Help();
+    IMenu *menu = new ConsoleMenu();
+    App app = App(menu, commands);
+    app.run();
 }
