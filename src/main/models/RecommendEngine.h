d1d2bf7c68684995cf73965279820d82bb6f3c32
aviv elkoni <avivpinhas.elkoni@live.biu.ac.il>
2024-11-21 11:14:29 +0200

    the code with algorithm for the RecommendEngine class

---
+++ src/main/models/RecommendEngine.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include "User.h"
+
+using namespace std;
+
+// Forward declarations of required classes and interfaces
+class Movie;
+class IUserService;
+
+class RecommendEngine {
+public:
+    RecommendEngine(IUserService* userService);
+
+   vector<int> getRecommendations(User& user, int movie);
+
+private:
+    IUserService* userService;
+// Function to compute the number of shared movies between two users
+int computeIntersect(const set<int>& UsrA, const set<int>& UsrB);
+
+// Function to compute the number of shared movies between the target user and all other users
+map<int, int> computeSharedMovies(const std::map<int, set<int>>& dataMap, int targetUserId);
+
+// Function to get a list of users who have watched a specific movie
+vector<int> WhoWatchedMovie(const std::map<int, set<int>>& dataMap, int targetMovieId);
+
+// Function to calculate the sum of shared movies for all users who watched a given movie
+map<int, int> SumOfSharedMovies(const std::map<int, set<int>>& dataMap, int movieId, int userId);
+};
