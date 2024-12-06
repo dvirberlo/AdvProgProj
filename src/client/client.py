5d62e287e5acf64bfbcf6a9b909f5ca32dc45a21
dorshroitman0812 <dorshroitman0803@gmail.com.com>
2024-12-06 10:39:47 +0200

    made the required changes : keep runing for ever , global const, args throw cmd , no addtional prints

--- src/client/client.py
+++ src/client/client.py
@@ -1,29 +1,26 @@
 import socket
 import sys
 
+MAXIMUM_BYTES_PER_MSG = 4096  # Define a constant for the buffer size
+
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <server_ip>")
-        sys.exit(1)
+    if len(sys.argv) != 3:
+        sys.exit(1)  
 
-    dest_ip = sys.argv[1]  # Get the server IP address from command line argument
-    dest_port = 8080
+    dest_ip = sys.argv[1]  # Get the server IP address from command line arguments
+    dest_port = int(sys.argv[2])  # Get the server port number from command line arguments
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        s.connect((dest_ip, dest_port))
+        socket.connect((dest_ip, dest_port))
     except ConnectionError as e:
-        print(f"Failed to connect to {dest_ip}:{dest_port}. Error: {e}")
+        print(f"Failed to connect to {dest_ip}:{dest_port}", file=sys.stderr)
         sys.exit(1)
-
-    msg = input("Message to send: ")
-    while msg != 'quit':
-        s.send(bytes(msg, 'utf-8'))
-        data = s.recv(4096)
-        print("Server sent: ", data.decode('utf-8'))
-        msg = input("Message to send: ")
-
-    s.close()
-
-if __name__ == '__main__':
+    # The client will keep sending messages to the server until the client is terminated by the user himself
+    while True:
+        msg = input()
+        socket.send(bytes(msg, 'utf-8'))
+        data = socket.recv(MAXIMUM_BYTES_PER_MSG)
+        print(data.decode('utf-8'))
+if __name__ == "__main__":
     main()
