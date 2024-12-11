28bfda286a207b6231f923841c05f42cded77b8a
dvirberlo <dvir.berl@gmail.com>
2024-12-11 19:52:30 +0200

    create Client Session

---
+++ src/server/main/Server/ClientSession.h
@@ -0,0 +1,29 @@
+#ifndef CLIENT_SESSION_H
+#define CLIENT_SESSION_H
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "../Commands/CommandParser.h"
+#include "../Commands/ICommand.h"
+#include "../Commands/IMenu.h"
+#include "../Executor/Runnable.h"
+
+using namespace std;
+
+class ClientSession : public Runnable {
+   private:
+    CommandParser& commandParser;
+    map<string, ICommand*>& commands;
+    int clientSocket;
+
+   public:
+    ClientSession(map<string, ICommand*>& commands,
+                  CommandParser& commandParser, int clientSocket);
+
+    void run() override;
+};
+
+#endif
