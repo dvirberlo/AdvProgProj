67949a759a777cc77d1c79f608e1e2749649651c
dorshroitman0812 <dorshroitman0803@gmail.com.com>
2024-12-18 18:02:57 +0200

    fixed the buffer size should work for any length of input

--- src/server/main/Server/ClientSession.cpp
+++ src/server/main/Server/ClientSession.cpp
@@ -15,7 +15,7 @@
 #include "../Commands/StatusCodeFactory.h"
 #include "../Executor/Runnable.h"
 
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 4098
 
 using namespace std;
 
@@ -29,19 +29,37 @@ ClientSession::ClientSession(map<string, ICommand*>& commands,
 void ClientSession::run() {
     char buffer[BUFFER_SIZE];
     while (true) {
-        int expectedDataLen = sizeof(buffer);
-        int readBytes = recv(this->clientSocket, buffer, expectedDataLen, 0);
-        if (readBytes == 0) {
-            // connection is closed
-            // note that in this exercise we assume that the client will never
-            // close the connection
-            return;
-        } else if (readBytes < 0) {
-            cout << "error reading from client" << endl;
-            return;
+        std::string inputCommand = "";
+        while (true) {
+            int expectedDataLen = sizeof(buffer);
+            int readBytes =
+                recv(this->clientSocket, buffer, expectedDataLen, 0);
+            if (readBytes == 0) {
+                // connection is closed
+                // note that in this exercise we assume that the client will
+                // never close the connection
+                return;
+            } else if (readBytes < 0) {
+                cout << "error reading from client" << endl;
+                return;
+            } else {
+                // Convert the buffer to a string : the command, concat the
+                // result to data already received .
+                inputCommand += std::string(buffer);
+                if (inputCommand.find("\n") != std::string::npos) {
+                    // Find the position of the newline character
+                    size_t newlinePos = inputCommand.find("\n");
+                    // Remove the newline character
+                    inputCommand = inputCommand.erase(newlinePos, 1);
+                    break;
+                } else {
+                    // Clear the buffer : ready for new data
+                    memset(buffer, 0, sizeof(buffer));
+                    continue;
+                }
+            }
         }
-        // Convert the buffer to a string : the command
-        std::string inputCommand(buffer);
+
         // Parse the command using the command parser
         vector<string> splittedCommand =
             this->commandParser.parseString(inputCommand);
