e70d90d69207db1bd8ecc585b2c85be36ba075ea
dorshroitman0812 <dorshroitman0803@gmail.com.com>
2024-12-25 11:14:09 +0200

    prettier

--- src/web-server/server.js
+++ src/web-server/server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 // Load environment variables
-require('custom-env').env(process.env.NODE_ENV, './config');
+require("custom-env").env(process.env.NODE_ENV, "./config");
 
 // Connect to MongoDB
-mongoose.connect(process.env.CONNECTION_STRING)
-    .then(() => console.log('MongoDB connected successfully'))
-    .catch(err => console.log('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 const server = express();
 
@@ -16,5 +17,5 @@ server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
 server.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
