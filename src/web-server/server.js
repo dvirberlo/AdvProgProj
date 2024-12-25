e74c37223979a7571c5b19f6818fc95de1115b3a
dorshroitman0812 <dorshroitman0803@gmail.com.com>
2024-12-25 10:56:09 +0200

    init server

---
+++ src/web-server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// Load environment variables
+require('custom-env').env(process.env.NODE_ENV, './config');
+
+// Connect to MongoDB
+mongoose.connect(process.env.CONNECTION_STRING)
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch(err => console.log('MongoDB connection error:', err));
+
+const server = express();
+
+server.use(express.urlencoded({ extended: true }));
+
+server.use(express.json());
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
