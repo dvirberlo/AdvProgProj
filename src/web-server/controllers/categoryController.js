e5ca402c24d5e358f8f0b78d6d01c852911b17ff
dorshroitman0812 <dorshroitman0803@gmail.com.com>
2024-12-25 18:14:16 +0200

    included 500 error msg

--- src/web-server/controllers/categoryController.js
+++ src/web-server/controllers/categoryController.js
@@ -12,14 +12,27 @@ const createCategory = async (req, res) => {
   return res.status(201).json(category);
 };
 const getCategories = async (req, res) => {
-  res.json.status(200)(await categoryService.getCategories());
+  try{
+    res.json.status(200)(await categoryService.getCategories());
+  }
+  catch(error){
+    console.error("categoryController: getCategories internal error:", error);
+    res.status(500).json({error: "Internal Server Error"});
+  }
+  
 };
 const getCategoryById = async (req, res) => {
+  try{
   const category = await categoryService.getCategoryById(req.params.id);
   if (!category) {
     return res.status(404).json({ error: "category not found" });
   }
   return res.json(category);
+}
+catch(error){
+  console.error("categoryController: getCategoryById internal error:", error);
+  res.status(500).json({error: "Internal Server Error"});
+};
 };
 const updateCategory = async (req, res) => {
   let category;
@@ -39,11 +52,18 @@ const updateCategory = async (req, res) => {
   return res.status(204).json({}); // return empty response
 };
 const deleteCategory = async (req, res) => {
-  const category = await categoryService.deleteCategory(req.params.id);
-  if (!category) {
-    return res.status(404).json({ error: "category not found" });
+  try{
+    const category = await categoryService.deleteCategory(req.params.id);
+    if (!category) {
+      return res.status(404).json({ error: "category not found" });
+    }
+    return res.status(204).json({}); // return empty response
   }
-  return res.status(204).json({}); // return empty response
+  catch(error){
+    console.error("categoryController: deleteCategory internal error:", error);
+    res.status(500).json({error: "Internal Server Error"});
+  }
+
 };
 module.exports = {
   createCategory,
