6a21a89ac7216d5b7f30c8d5735d594f35bf4b42
avivelkoni <avivb77@gmail.com>
2024-12-26 13:59:58 +0200

    format the files

--- src/web-server/routes/recommendRouter.js
+++ src/web-server/routes/recommendRouter.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 var router = express.Router();
-const recommendController = require('../controllers/recommendController');
+const recommendController = require("../controllers/recommendController");
 
-router.route('/:id/recommend')
-    .get(recommendController.getRecommendations)
-    .post(recommendController.addWatch);
+router
+  .route("/:id/recommend")
+  .get(recommendController.getRecommendations)
+  .post(recommendController.addWatch);
 
 module.exports = router;
