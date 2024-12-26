599434afa4a080c8010876ec881f5645cb04980e
dvirberlo <dvir.berl@gmail.com>
2024-12-26 22:38:36 +0200

    add search by category

--- src/web-server/services/movieSearchService.js
+++ src/web-server/services/movieSearchService.js
@@ -1,13 +1,27 @@
+const mongoose = require("mongoose");
 const Movie = require("../models/movieModel");
+const Category = require("../models/categoryModel");
 
 /**
  * @param {String?} query
  * @return movies that has a field that match the specified query
  */
 const searchMovies = async (query) => {
-  return await Movie.find({
-    $or: [{ name: query }, { releaseDate: query }],
-  });
+  const matches = [];
+  if (typeof query === "string") {
+    matches.push(...(await Movie.find({ name: query })));
+
+    const category = await Category.findOne({ name: query });
+    if (category != null)
+      matches.push(
+        ...(await Movie.find({ categories: { $in: category._id } }))
+      );
+  }
+  if (!isNaN(new Date(query))) {
+    matches.push(...(await Movie.find({ date: query })));
+  }
+  // return without duplicates
+  return Array.from(new Map(matches.map((item) => [item.id, item])).values());
 };
 
 module.exports = {
