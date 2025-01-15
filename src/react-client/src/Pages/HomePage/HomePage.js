d2fff2879c28937cb864bb1b38394f01c6b53acd
dorshroitman0812 <dorshroitman0803@gmail.com.com>
2025-01-15 23:00:12 +0200

    movieDetail component first version before any http req

--- src/react-client/src/Pages/HomePage/HomePage.js
+++ src/react-client/src/Pages/HomePage/HomePage.js
@@ -1,47 +1,24 @@
 import { useState } from "react";
 import { NavBarTemplate } from "../../Components/NavBar/NavBarTemplate";
+import { MovieDetails } from "../../Components/MovieDetails/MovieDetails";
+import img from "./img.jpg";
+import { getMoviesHttp } from "../../HttpRequest/getMovieHttp";
 
 export const HomePage = () => {
-  const [counter, setCounter] = useState(0);
-
+  // const movie = {
+  //   imageUrl: img,
+  //   title: "movie",
+  //   description: "hello team...",
+  //   rating: "★★★★☆",
+  //   year: "2025",
+  //   genre: "Drama",
+  // };
+  //const [counter, setCounter] = useState(0);
+  movie = getMoviesHttp();
   return (
     <NavBarTemplate>
-      <div className="container">
-        <h1 className="text-center">Home Page</h1>
-        <button
-          onClick={() => setCounter(counter + 1)}
-          className="btn btn-primary"
-        >
-          Clicked #{counter} times
-        </button>
-        <div
-          className="alert alert-warning alert-dismissible fade show"
-          role="alert"
-        >
-          <strong>Holy guacamole!</strong> You should check in on some of those
-          fields below.
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="alert"
-            aria-label="Close"
-          ></button>
-        </div>
-        {Array.from({ length: 20 }).map((item, i) => (
-          <p key={i}>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
-            packages and web page editors now use Lorem Ipsum as their default
-            model text, and a search for 'lorem ipsum' will uncover many web
-            sites still in their infancy. Various versions have evolved over the
-            years, sometimes by accident, sometimes on purpose (injected humour
-            and the like).
-          </p>
-        ))}
-      </div>
+      <MovieDetails movie={movie} />
+      <div className="container"></div>
     </NavBarTemplate>
   );
 };
