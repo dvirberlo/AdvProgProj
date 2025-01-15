db2d665c61d10e70605d59bbb516b22e8ec66b7a
dvirberlo <dvir.berl@gmail.com>
2025-01-15 11:11:17 +0200

    use bootstrap

--- src/react-client/src/HomePage/HomePage.js
+++ src/react-client/src/HomePage/HomePage.js
@@ -5,11 +5,27 @@ import "./HomePage.css";
 export const HomePage = () => {
   const [counter, setCounter] = useState(0);
   return (
-    <div>
-      Home Page Component
-      <button onClick={() => setCounter(counter + 1)}>
+    <div className="container">
+      <h1 className="text-center">Home Page Component</h1>
+      <button
+        onClick={() => setCounter(counter + 1)}
+        className="btn btn-primary"
+      >
         Clicked #{counter} times
       </button>
+      <div
+        className="alert alert-warning alert-dismissible fade show"
+        role="alert"
+      >
+        <strong>Holy guacamole!</strong> You should check in on some of those
+        fields below.
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
     </div>
   );
 };
