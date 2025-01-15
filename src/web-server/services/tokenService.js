379fefe117c1940ac2bce2027d988fe71b930f68
dorshroitman0812 <dorshroitman0803@gmail.com.com>
2025-01-15 17:44:54 +0200

    include jwt token service

--- src/web-server/services/tokenService.js
+++ src/web-server/services/tokenService.js
@@ -1,20 +1,52 @@
+const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const UserService = require("../services/userService");
+const { prettyFormat } = require("@testing-library/react");
+/**
+ *
+ * @param {*} userId : the mongo id of the user
+ * @returns : returning the user document if the token is valid, otherwise null
+ */
+const verifyToken = async (token) => {
+  // Validate token
+  if (!token) {
+    return null;
+  }
+  // Validate token
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+  } catch (error) {
+    //console.error("JWT verification failed:", error.message);
+    return null;
+  }
 
-const User = require("../models/userModel");
+  return userDocument;
+};
 
 /**
  *
- * @param {String?} username
- * @param {String?} password
- * @returns User's id as ObjectId with the specified username and password, or null if doesn't exist
- * @throws on DB connection errors
+ * @param {*} userId : the mongo id of the user
+ * @returns : the token (jwt) digital signature
  */
-const getToken = async (username, password) => {
-  const user = await User.findOne({ username: username, password: password });
-  if (!user) return null;
-  return user._id;
+const createToken = async (userId) => {
+  // Validate userId
+  if (!mongoose.isValidObjectId(userId)) {
+    return null;
+  }
+
+  // Check if the user exists
+  const userDocument = await UserService.getUserById(userId);
+  if (!userDocument) {
+    return null;
+  }
+  const userData = userDocument.toJSON({ versionKey: false });
+  const token = jwt.sign(userData, process.env.JWT_PRIVATE_KEY);
+  return token;
 };
 
 module.exports = {
-  getToken,
+  createToken,
+  verifyToken,
 };
