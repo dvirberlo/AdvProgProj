126cbd192d30056b8c86e728281f1bceed81485a
dvirberlo <dvir.berl@gmail.com>
2025-01-15 11:11:05 +0200

    add bootstrap

--- src/react-client/src/index.js
+++ src/react-client/src/index.js
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { AppRouter } from "./AppRouter/AppRouter";
 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
