8c8094b6607c6cc6b29dfab49f4e56984e591d32
dvirberlo <dvir.berl@gmail.com>
2025-01-16 17:23:58 +0200

    add form input components

---
+++ src/react-client/src/Components/FormInputs/FormInputs.js
@@ -0,0 +1,106 @@
+export const FileInput = ({ name, text, accept, required }) => {
+  return (
+    <div className="mb-3">
+      <label className="form-label">{text}</label>
+      <input
+        type="file"
+        accept={accept}
+        className="form-control"
+        name={name}
+        required={required}
+      />
+    </div>
+  );
+};
+
+export const TextInput = ({
+  name,
+  text,
+  maxLength,
+  required,
+  onChange,
+  value,
+}) => {
+  return (
+    <div className="mb-3">
+      <label className="form-label">{text}</label>
+      <input
+        type="text"
+        className="form-control"
+        name={name}
+        maxLength={maxLength}
+        required={required}
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  );
+};
+
+export const NumberInput = ({
+  name,
+  text,
+  min,
+  max,
+  required,
+  onChange,
+  value,
+}) => {
+  return (
+    <div className="mb-3">
+      <label className="form-label">{text}</label>
+      <input
+        type="number"
+        className="form-control"
+        name={name}
+        min={min}
+        max={max}
+        required={required}
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  );
+};
+
+export const TextareaInput = ({
+  name,
+  text,
+  maxLength,
+  required,
+  onChange,
+  value,
+}) => {
+  return (
+    <div className="mb-3">
+      <label className="form-label">{text}</label>
+      <textarea
+        type="text"
+        className="form-control"
+        name={name}
+        maxLength={maxLength}
+        required={required}
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  );
+};
+
+export const CheckboxInput = ({ name, text, onChange, checked }) => {
+  return (
+    <div className="form-check mb-3">
+      <input
+        type="checkbox"
+        className="form-check-input"
+        id={`${name}_CheckboxInput`}
+        name={name}
+        onChange={onChange}
+        checked={checked}
+      />
+      <label className="form-check-label" htmlFor={`${name}_CheckboxInput`}>
+        {text}
+      </label>
+    </div>
+  );
+};
