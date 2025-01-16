e59a5020d18159daf5fd8aa6275f1b32fa1aa708
dvirberlo <dvir.berl@gmail.com>
2025-01-16 17:24:27 +0200

    create admin forms

---
+++ src/react-client/src/Pages/AdminPage/CreateCategoryForm.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import {
+  TextInput,
+  CheckboxInput,
+} from "../../Components/FormInputs/FormInputs";
+
+export const CreateCategoryForm = () => {
+  const [name, setName] = useState("");
+  const [promoted, setPromoted] = useState(false);
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3000/api/categories", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, promoted }),
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          setError("");
+          setSuccess(true);
+          setName("");
+          setPromoted(false);
+        } else if (response.status === 409) {
+          setError("Category already exists");
+          setSuccess(false);
+        } else {
+          const data = await response.json();
+          setError(data.error);
+          setSuccess(false);
+        }
+      })
+      .catch((error) => {
+        setError("Server error");
+        setSuccess(false);
+        console.error("Error:", error);
+      });
+  };
+  return (
+    <form onSubmit={onSubmit} encType="multipart/form-data">
+      <legend>Create Category</legend>
+      <TextInput
+        name="name"
+        text="Name"
+        maxLength={20}
+        required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <CheckboxInput
+        name="promoted"
+        text="Promoted"
+        checked={promoted}
+        onChange={(e) => setPromoted(e.target.checked)}
+      />
+      <button type="submit" className="btn btn-danger">
+        Submit
+      </button>
+      {error && <div className="my-2 alert alert-danger">{error}</div>}
+      {success && (
+        <div className="my-2 alert alert-success">Category created</div>
+      )}
+    </form>
+  );
+};
---
+++ src/react-client/src/Pages/AdminPage/EditMovieForm.js
@@ -0,0 +1,3 @@
+export const EditMovieForm = () => {
+  return <div>TODO: Edit Movie</div>;
+};
