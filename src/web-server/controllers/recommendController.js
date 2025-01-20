3e4c7c48be6f7cde1b81f9cfadd2396abea8b34a
dvirberlo <dvir.berl@gmail.com>
2025-01-20 17:39:54 +0200

    add recommendation to movie page and small fixes

--- src/web-server/controllers/recommendController.js
+++ src/web-server/controllers/recommendController.js
@@ -1,18 +1,19 @@
 const recommendService = require("../services/recommendService");
+const movieService = require("../services/movieService");
+const tokenService = require("../services/tokenService");
 const { TOKEN_ID_HEADER } = require("../constants/httpHeaders");
 
 // Controller function to get recommendations
 const getRecommendations = async (req, res) => {
-  const userId = req.headers[TOKEN_ID_HEADER];
+  // check if the existing user having that token-id
+  const user = await tokenService.verifyToken(req.headers[TOKEN_ID_HEADER]);
+  if (user === null)
+    return res.status(401).json({ error: "User token is invalid" });
+
   const movieId = req.params.id;
-
-  if (!userId) {
-    return res.status(401).json({ error: "Token-ID header is missing" });
-  }
-
   try {
     const recommendations = await recommendService.getRecommendations(
-      userId,
+      user.id,
       movieId
     );
     if (recommendations.error) {
@@ -20,29 +21,35 @@ const getRecommendations = async (req, res) => {
         .status(recommendations.status)
         .json({ error: recommendations.error });
     }
-    return res.status(200).json({ recommendation: recommendations.data });
+    // populate the response with the recommended movies
+    const recommendedMovies = [];
+    for (const movieId of recommendations.data) {
+      recommendedMovies.push(await movieService.getMovieById(movieId));
+    }
+    return res.status(200).json(recommendedMovies);
   } catch (error) {
     console.error("Error in getRecommendations controller:", error);
     return res.status(500).json({ error: "Internal server error" });
   }
 };
 
-const addWatch = async (req, res) => {
-  const userId = req.headers[TOKEN_ID_HEADER];
+const addWatch = async (req, res) => {  
+  // check if the existing user having that token-id
+  const user = await tokenService.verifyToken(req.headers[TOKEN_ID_HEADER]);
+  if (user === null)
+    return res.status(401).json({ error: "User token is invalid" });
+
   const movieId = req.params.id;
-  if (!userId) {
-    return res.status(401).json({ error: "Token-ID header is missing" });
-  }
 
   try {
     // Call the service function to add watch
-    const result = await recommendService.addWatch(userId, movieId);
+    const result = await recommendService.addWatch(user.id, movieId);
 
     // Return the result based on the response from the service
     if (result.status === 201) {
       return res
         .status(201)
-        .json({ watcher: `${userId}`, movie: `${movieId}` });
+        .json({ watcher: `${user.id}`, movie: `${movieId}` });
     } else if (result.status === 204) {
       return res.status(204).json();
     } else {
